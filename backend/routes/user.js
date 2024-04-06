8a3b259c58555fcf1dc4edfe430be353f8d58ab8
tanishka <tsdineshjai@gmail.com>
2024-04-06 17:45:43 +0530

    sign in handling has been done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const bcrypt = require("bcrypt");
+
 //creating a User Schema for zod input validation
 
 const UserSchema = z.object({
@@ -16,6 +18,7 @@ const UserSchema = z.object({
 	lastName: z.string(),
 });
 
+//to sign up
 router.post("/signup", async (req, res) => {
 	const { username, password, firstName, lastName } = req.body;
 
@@ -59,6 +62,47 @@ router.post("/signup", async (req, res) => {
 				message: `User:${findUser?.username} already exists`,
 			});
 		}
+	}
+});
+
+const LoginSchema = z.object({
+	username: z.string().email(),
+	password: z.string(),
+});
+
+//to sign in
+router.post("/signin", async (req, res) => {
+	const { username, password } = req.body;
+
+	//check if the username and password matches the record in the database
+	const loginInputValidation = LoginSchema.safeParse({ username, password });
+
+	if (!loginInputValidation.success) {
+		res.status(411).json({
+			message: "wrong type of inputs",
+		});
+	}
+
+	//check the record from database
+	const dbrecord = await User.findOne({ username });
+
+	if (dbrecord) {
+		const isPasswordMatching = await bcrypt.compare(
+			password,
+			dbrecord.password
+		);
+
+		if (isPasswordMatching) {
+			const userId = dbrecord._id;
+			const token = jwt.sign({ userId }, JWT_SECRET);
+			res.status(200).json({
+				token,
+			});
+		}
+	} else {
+		res.status(411).json({
+			messsage: `No such record has been found`,
+		});
 	}
 });
 
